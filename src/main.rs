ac0fcfd1b73a669279d6b9a1d1e6bd8df65b5afd
Lexi Blesius <Ablesius@users.noreply.github.com>
2025-04-25 11:33:38 +0200

    refactor: Missing link error
    This makes the error more explicit, exits gracefully with error code,
    and is clearer in intention.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use html_link_to_text::Link;
+use std::process::exit;
 
 fn main() {
     // 1. read text from command line!
     let args: Vec<String> = std::env::args().collect();
 
     // args must not be empty!
-    if args.len() != 2 {
-        panic!("You have to provide a link to the program!");
+    if args.len() < 2 || args[1].is_empty() {
+        eprintln!("You have to provide a link to the program!");
+        exit(1);
     }
     // 2. strip down to url - for the moment assume we'll just paste the url to the tool, nothing
     //    more!
